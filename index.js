32a16ec55065af00eda24208c9ca5af3a0186478
sanket bhalerao <sankbh44@in.ibm.com>
2017-08-07 13:46:26 +0530

    change in hello world msg

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let http = require('http');
 
 let server = http.createServer((request, response) => {
-    response.write("hello world");
+    response.write("hello world " + process.env.PORT);
     response.end();
 });
 
 server.listen(process.env.PORT);
-console.log('listening on port');
+console.log('listening on port '+ process.env.PORT);
